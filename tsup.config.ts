8d461a9ef111a7fecb81d85930c066f6f0ac96f1
Avaer Kazmer <ultimus@gmail.com>
2025-02-07 14:58:36 -0800

    Externalize core

--- tsup.config.ts
+++ tsup.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
         "http",
         "agentkeepalive",
         "fluent-ffmpeg",
+        "@elizaos/core"
         // Add other modules you want to externalize
     ],
 });
